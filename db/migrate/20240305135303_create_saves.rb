55b69b7bab8d93f11f399173122cde8889119eec
Andy Tutton <andytutton2@gmail.com>
2024-03-05 13:55:37 +0000

    save model done

---
+++ db/migrate/20240305135303_create_saves.rb
@@ -0,0 +1,10 @@
+class CreateSaves < ActiveRecord::Migration[7.1]
+  def change
+    create_table :saves do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :activity, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_113644) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_135303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_113644) do
     t.index ["group_id"], name: "index_proposals_on_group_id"
   end
 
+  create_table "saves", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "activity_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_saves_on_activity_id"
+    t.index ["user_id"], name: "index_saves_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -72,4 +81,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_05_113644) do
   add_foreign_key "groups", "users"
   add_foreign_key "proposals", "activities"
   add_foreign_key "proposals", "groups"
+  add_foreign_key "saves", "activities"
+  add_foreign_key "saves", "users"
 end
