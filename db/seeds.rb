e1b3114a8e55a9d4da188d964384d7616552b5ce
Jacek Bastin <jacek.bastin@gmail.com>
2024-03-06 16:24:58 +0000

    User can leave a group

--- db/seeds.rb
+++ db/seeds.rb
@@ -71,3 +71,5 @@ Activity.destroy_all
 puts "generated activities"
 
 User.create(first_name: "Akash", last_name: "Zaveri", email: "akazaveri@gmail.com", password: "password")
+User.create(first_name: "Jacek", last_name: "Bastin", email: "jacek@gmail.com", password: "password")
+User.create(first_name: "Nicolas", last_name: "Ayoub", email: "nicolas@gmail.com", password: "password")
