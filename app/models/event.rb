4163b0b6078e7e68489020c4c4ebea748655e729
Akash Zaveri <akazaveri@gmail.com>
2024-03-08 13:17:16 +0000

    added bookings

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :group
   belongs_to :activity
-  has_many :attendances
+  has_many :attendances, dependent: :destroy
   has_many :users, through: :attendances
 end
