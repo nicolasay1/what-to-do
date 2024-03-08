1c3e8086ea8f8cd3719e6d197b5674792a1b9534
Jacek Bastin <jacek.bastin@gmail.com>
2024-03-08 12:37:16 +0000

    added upload avatar/edit avatar, avatar on group page and on add member page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :created_groups, class_name: 'group'
   has_many :attendances
   has_many :likes
+  has_one_attached :avatar
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
