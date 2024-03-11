64a2b0e6c59cc3abaf129762df21b9c7832b65ac
Akash Zaveri <akazaveri@gmail.com>
2024-03-11 12:53:36 +0000

    added the chatroom

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,8 +12,9 @@ class GroupsController < ApplicationController
     @members = @group.users
     @invite_link = "#{request.original_url}/memberships/new?invite_token=#{@group.invite_token}"
     @proposals = Proposal.where(group_id: @group.id)
-    @events = Event.where(group_id: @group.id, booked: false)
+    @events = Event.where(group_id: @group.id, booked: nil)
     @bookings = Event.where(group_id: @group.id, booked: true).order(date: :asc)
+    @chatroom = @group.chatroom
   end
 
   def create
