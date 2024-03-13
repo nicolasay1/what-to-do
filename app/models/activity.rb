c96b484e79aa606d07e628cdfa9d82f64aa31996
Nicolas Ayoub <nicolas.ayoub1@gmail.com>
2024-03-13 11:19:38 +0000

    Styled filter button, added a distance filter for activities, changed lat and lon names in schema

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,5 @@ class Activity < ApplicationRecord
   has_many :events
   has_many :saves
   has_many_attached :photos
+  geocoded_by :address
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_175239) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_13_110653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,8 +52,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_175239) do
     t.text "tags"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "lat"
-    t.float "lng"
+    t.float "latitude"
+    t.float "longitude"
     t.time "end_time"
     t.string "price"
     t.string "people"
