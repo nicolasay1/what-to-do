2406e1b69a37ab5b3a65e85e39ea2900b5b3e32f
Jacek Bastin <jacek.bastin@gmail.com>
2024-03-13 20:42:02 +0000

    styling + added to seed

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,7 @@ class Activity < ApplicationRecord
   has_many :saves
   has_many_attached :photos
   geocoded_by :address
+
+  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+
 end
