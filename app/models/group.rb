c24d29cddf8bae31f0d0ab38b91159b773a94f0a
Jacek Bastin <jacek.bastin@gmail.com>
2024-03-13 18:11:14 +0000

    dark-map-destroy-group-fix

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,12 +3,9 @@ class Group < ApplicationRecord
   has_many :users, through: :memberships
   has_many :events, dependent: :destroy
   belongs_to :user
-  has_one :chatroom
-
+  has_one :chatroom, dependent: :destroy
   validates :name, presence: true
-
   has_secure_token :invite_token
-
   has_one_attached :banner
 
 end
